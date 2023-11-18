c8963c74fe0d3daac7a7b42998cf539ec3e53459
menshaha <150454136+menshaha@users.noreply.github.com>
2023-11-19 00:07:06 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -44,13 +44,53 @@ local Autoplay = Window:NewTab("AUTOPLAY")
 local Macro = Window:NewTab("MACRO")
 
 --MISC
-local Misc = Window:NewTab("Misc")
+local Misc = Window:NewTab("MISC")
 
-local TeamSection = Main:NewSection("TEAMS")
+local TeamSection = Misc:NewSection("TEAMS")
 
 TeamSection:NewButton("Team 1", "Use for equip team 1", function()
     local args = {
         [1] = "1"
+    }
+    
+    game:GetService("ReplicatedStorage").endpoints.client_to_server.switch_team_loadout:InvokeServer(unpack(args))
+end)
+
+TeamSection:NewButton("Team 2", "Use for equip team 2", function()
+    local args = {
+        [1] = "2"
+    }
+    
+    game:GetService("ReplicatedStorage").endpoints.client_to_server.switch_team_loadout:InvokeServer(unpack(args))
+end)
+
+TeamSection:NewButton("Team 3", "Use for equip team 3", function()
+    local args = {
+        [1] = "3"
+    }
+    
+    game:GetService("ReplicatedStorage").endpoints.client_to_server.switch_team_loadout:InvokeServer(unpack(args))
+end)
+
+TeamSection:NewButton("Team 4", "Use for equip team 4", function()
+    local args = {
+        [1] = "4"
+    }
+    
+    game:GetService("ReplicatedStorage").endpoints.client_to_server.switch_team_loadout:InvokeServer(unpack(args))
+end)
+
+TeamSection:NewButton("Team 5", "Use for equip team 5", function()
+    local args = {
+        [1] = "5"
+    }
+    
+    game:GetService("ReplicatedStorage").endpoints.client_to_server.switch_team_loadout:InvokeServer(unpack(args))
+end)
+
+TeamSection:NewButton("Team 6", "Use for equip team 6", function()
+    local args = {
+        [1] = "6"
     }
     
     game:GetService("ReplicatedStorage").endpoints.client_to_server.switch_team_loadout:InvokeServer(unpack(args))
